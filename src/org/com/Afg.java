93b28af6237d8bf2d8360992858fe240159427ac
ArvinthanMSD0713 <138639353+ArvinthanMSD0713@users.noreply.github.com>
2023-07-14 18:24:11 +0530

    Update Afg.java

--- src/org/com/Afg.java
+++ src/org/com/Afg.java
@@ -8,9 +8,10 @@ public class Afg {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\RAMESH\\eclipse-workspace\\Selenium\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://greenstech.in/selenium-course-content.html");
+		driver.manage().window().maximize();
 		WebElement txt = driver.findElement(By.xpath("//div[@id='heading20']"));
 		txt.click();
-		WebElement txt1 = driver.findElement(By.xpath("//a[@href='http://traininginchennai.in/interview-questions/cts.pdf#toolbar=0']"));
+		WebElement txt1 = driver.findElement(By.xpath("//a[contains(text(),'CTS Interview Question')]"));
 		txt1.click();
 	}
 }
